f71dbb91184077b2200d4eed5f0197b2c5ea22b4
Sivarajan <citysiva180@gmail.com>
2020-09-01 19:03:37 +0530

    Completed :-)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import login from "../src/components/login/login";
 // import BucketView from "../src/components/BucketView/BucketView";
 // import HelpPage from "../src/components/HelpPage/HelpPage";
 // import Todo from "../src/components/Todolist/Todolist";
-export const CredentialsContext = React.createContext({});
+export const CredentialsContext = React.createContext();
 
 function App() {
   const credentialsState = useState(null);
